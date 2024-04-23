32311a805b4649141444b8833c517c905be017b1
David Lee <Dai180293@gmail.com>
2024-04-23 12:36:35 +0100

    FIRST COMMIT

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <main>
+            <h1>
+                Generate a <span>free</span> secure password
+            </h1>
+        </main>
+    );
+}
+
+export default App;
